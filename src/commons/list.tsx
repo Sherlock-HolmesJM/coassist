8316bf16b514803f5a761c2c69e6277371302ae9
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-03-13 13:00:35 -0800

    members

--- src/commons/list.tsx
+++ src/commons/list.tsx
@@ -1,25 +1,47 @@
-import { capitalize } from "../util";
-import styled from "styled-components";
+import { capitalize } from '../util';
+import styled from 'styled-components';
+import { MemberI } from '../components/members';
 
 export interface ListProps {
-  items: string[];
+  items: MemberI[];
   title: string;
+  onDelete?: (name: string) => void;
+  onMark?: (member: MemberI) => void;
 }
 
-const List: React.SFC<ListProps> = (props) => {
-  const { title, items } = props;
+const List: React.FC<ListProps> = (props) => {
+  const { title, items, onMark, onDelete } = props;
+  const ts = items.filter((m) => m.type === 'T');
+  const tes = items.filter((m) => m.type === 'TE');
+  const sorted = [...ts, ...tes];
 
   return (
-    <Div>
-      <h3 className="title">{capitalize(title)}</h3>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item">
-            {capitalize(item)}
-            <div>
-              <span className="badge m-2 bg-success">M</span>
-              <span className="badge bg-danger">X</span>
-            </div>
+    <Div className='list'>
+      <h3 className='title'>{capitalize(title)} </h3>
+      <div className='badge-container'>
+        <span className='badge bg-warning m-1'>Ts: {ts.length}</span>
+        <span className='badge bg-warning'>TEs: {tes.length}</span>
+      </div>
+      <ul className='list-group'>
+        {sorted.map((item) => (
+          <li className='list-group-item' key={item.name}>
+            {capitalize(item.name)} - {item.type}
+            {onMark && onDelete && (
+              <div>
+                <span
+                  className='badge m-2 bg-success'
+                  onClick={() => onMark(item)}
+                >
+                  M
+                </span>
+                <span
+                  className='badge bg-danger'
+                  onClick={() => onDelete(item.name)}
+                >
+                  X
+                </span>
+              </div>
+            )}
           </li>
         ))}
       </ul>
@@ -28,8 +50,17 @@ const List: React.SFC<ListProps> = (props) => {
 };
 
 const Div = styled.div`
+  margin: 10px;
+  position: relative;
+
+  .badge-container {
+    position: absolute;
+    top: 25px;
+    right: 1px;
+  }
   .title {
     font-size: clamp(1.2rem, 4vw, 1.75rem);
+    padding: 5px;
   }
   li {
     display: flex;
@@ -39,6 +70,12 @@ const Div = styled.div`
 
   .badge {
     cursor: pointer;
+  }
+
+  @media print {
+    .title {
+      font-size: 1.2rem;
+    }
   }
 `;
 
--- src/components/members.tsx
+++ src/components/members.tsx
@@ -1,45 +1,110 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import List from "../commons/list";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import List from '../commons/list';
+import { Link } from 'react-router-dom';
+import { capitalize } from '../util';
 
 export interface MembersProps {}
+export type MemberStatus = 'active' | 'inactive';
+export type MemberType = 'TE' | 'T';
+export interface MemberI {
+  name: string;
+  type: MemberType;
+  status: MemberStatus;
+}
 
 const Members: React.FC<MembersProps> = (props) => {
-  const [list, setList] = useState<string[]>(["ugochukwu"]);
-  const [members, setMembers] = useState<string[]>(["sister ibim"]);
-  const [member, setMember] = useState("");
+  const [members, setMembers] = useState<MemberI[]>([]);
+  const [name, setName] = useState('');
+  const [type, setType] = useState<MemberType>('T');
+
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  const activeMembers = members.filter((m) => m.status === 'active');
+  const inactiveMembers = members.filter((m) => m.status === 'inactive');
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (member) {
-      const l = [...list];
-      l.push(member);
-      setList(l);
-    }
+
+    const result = members.find((m) => m.name === name);
+    if (result) return alert(`${capitalize(name)} is already a member.`);
+
+    const newMember: MemberI = {
+      name,
+      type,
+      status: 'inactive',
+    };
+    const newMembers = [...members];
+    newMembers.push(newMember);
+    setMembers(newMembers);
+    setName('');
+    nameRef.current?.focus();
+  };
+
+  const handleMark = (member: MemberI) => {
+    const newMember = { ...member };
+    newMember.status = newMember.status === 'active' ? 'inactive' : 'active';
+    const newMembers = members.filter((m) => m.name !== member.name);
+    newMembers.push(newMember);
+    setMembers(newMembers);
+  };
+
+  const handleDelete = (name: string) => {
+    const result = prompt('Are you sure?');
+    if (result === null) return;
+    const newMembers = members.filter((m) => m.name !== name);
+    setMembers(newMembers);
   };
 
   return (
     <Section>
-      <header className="header">
-        <nav className="nav">
-          <Link to="/home" className="btn btn-link">
+      <header className='header'>
+        <nav className='nav'>
+          <Link to='/home' className='btn btn-link'>
             back
           </Link>
         </nav>
-        <form onSubmit={handleSubmit} className="form">
+        <form onSubmit={handleSubmit} className='form'>
           <input
-            className="form-control"
-            type="text"
+            className='form-control'
+            type='text'
             placeholder="member's name"
-            onChange={(e) => setMember(e.target.value.toLowerCase())}
+            value={name}
+            onChange={(e) => setName(e.target.value.toLowerCase())}
+            required
+            ref={nameRef}
           />
-          <input className="btn btn-primary" type="submit" value="Add" />
+          <select
+            name='type'
+            id='type'
+            required
+            className='form-select'
+            onChange={(e) => setType(e.target.value as MemberType)}
+          >
+            <option value='T'>T</option>
+            <option value='TE'>TE</option>
+          </select>
+          <input className='btn btn-primary' type='submit' value='Add' />
         </form>
       </header>
-      <div className="container">
-        <List items={list} title="inactive members" />
-        <List items={members} title="active members" />
+      <div className='container'>
+        <List
+          items={inactiveMembers}
+          title='inactive members'
+          onMark={handleMark}
+          onDelete={handleDelete}
+        />
+        <List
+          items={activeMembers}
+          title='active members'
+          onMark={handleMark}
+          onDelete={handleDelete}
+        />
+      </div>
+      <div className='hide'>
+        <List items={members} title='all members' />
+        <List items={activeMembers} title='active members' />
+        <List items={inactiveMembers} title='inactive members' />
       </div>
     </Section>
   );
@@ -73,8 +138,24 @@ const Section = styled.section`
     justify-content: center;
   }
   .container > * {
-    margin: 20px;
     flex-basis: 400px;
+  }
+  .hide {
+    display: none;
+    flex-direction: column;
+    justify-content: flex-start;
+    border: 1px solid blue;
+    margin: 10px;
+  }
+
+  @media print {
+    .container,
+    .header {
+      display: none;
+    }
+    .hide {
+      display: flex;
+    }
   }
 `;
 
