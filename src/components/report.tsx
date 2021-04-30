113735938bdec0d2cbd2c376ceb6e2091108ed82
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-04-30 13:41:35 +0100

    added functions to work with date

--- src/components/report.tsx
+++ src/components/report.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext, useRef } from 'react';
 import { context } from '../context/context';
 import { Worker } from '../types';
 import styled from 'styled-components';
